ebfe38ccbe2d9227a797574259320869187a5e68
chrmysle <13384688816@163.con>
2026-08-28 23:38:58 +0800

    feat: add trapping rain water solution

---
+++ solutions/cpp/catch_the_rain_42.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int total = 0;
+        int maxHeight = *max_element(height.begin(),height.end());
+        int length = height.size();
+        for(int i = 1; i <= maxHeight; i++){
+            int j = 0;
+            while(j < length && height[j] - i < 0){
+                j++;
+            }
+            int k = length - 1;
+            while(k >= 0 && height[k] - i < 0){
+                k--;
+            }
+            for(int p = j; p < k; p++){
+                if(height[p] - i < 0) total++;
+            }
+        }
+
+        return total;
+    }
+};

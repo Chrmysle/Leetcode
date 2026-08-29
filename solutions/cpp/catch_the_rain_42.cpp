22697cbac0a8acbff940b5c944931b40123b3b15
chrmysle <13384688816@163.con>
2026-08-29 11:32:58 +0800

    catch_the_rain_42

--- solutions/cpp/catch_the_rain_42.cpp
+++ solutions/cpp/catch_the_rain_42.cpp
@@ -29,6 +29,36 @@ class Solution_02 {
 public:
     int trap(vector<int>& height) {
         int total = 0;
+
+        int n = height.size();
+
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+
+        leftMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
+        }
+
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            total += min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return total;
+    }
+};
+
+class Solution_03 {
+public:
+    int trap(vector<int>& height) {
+        int total = 0;
         int left = 0;
         int right = height.size() - 1;
 
